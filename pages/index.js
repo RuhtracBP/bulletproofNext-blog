980b374fa5b34c3580552f2d6c0d589717466a5e
ArthurWSL <arthurhcbp@gmail.com>
2021-05-12 17:30:31 -0300

    my final blog

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,48 @@
-import Link from 'next/link'
-import Theme from '../components/Theme'
-import ms from 'ms'
+import Link from "next/link";
+import Theme from "../components/Theme";
+import ms from "ms";
+import { promises as fsPromises } from "fs";
 
-export default function Home ({ postList }) {
+export default function Home({ postList }) {
   return (
     <Theme>
-      <div className='post-list'>
-        {postList.map(post => (
-          <div key={post.slug} className='post-link'>
-            <Link href='/post/[slug]' as={`/post/${post.slug}`}>
+      <div className="post-list">
+        {postList.map((post) => (
+          <div key={post.slug} className="post-link">
+            <Link href="/post/[slug]" as={`/post/${post.slug}`}>
               <a>
-                <div className='time'>{ms(Date.now() - post.createdAt, { long: true })} ago</div>
-                <div className='title'>{post.title}</div>
+                <div className="time">
+                  {ms(Date.now() - post.createdAt, { long: true })} ago
+                </div>
+                <div className="title">{post.title}</div>
               </a>
             </Link>
           </div>
         ))}
       </div>
     </Theme>
-  )
+  );
 }
 
-export async function getStaticProps () {
-  const postList = [
-    {
-      slug: '2020-July-01-Hello-World',
-      title: 'Hello World',
-      createdAt: (new Date('2020 July 01')).getTime()
-    }
-  ]
+export async function getStaticProps() {
+  const markdownFiles = await fsPromises.readdir("data");
+
+  const postList = markdownFiles.map((filename) => {
+    const slug = filename.replace(/.md$/, "");
+    const [year, month, date, ...rest] = slug.split("-");
+    const createdAt = new Date(`${year} ${month} ${date}`).getTime();
+    const title = rest.join(" ");
+
+    return {
+      slug,
+      createdAt,
+      title,
+    };
+  });
 
   return {
     props: {
-      postList
-    }
-  }
+      postList,
+    },
+  };
 }
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,44 +1,63 @@
-import Theme from '../../components/Theme'
-import ms from 'ms'
+import Theme from "../../components/Theme";
+import ms from "ms";
+import { promises as fsPromises } from "fs";
+import Markdown from "markdown-to-jsx";
+import Youtube from "../../components/Youtube";
 
-export default function Post ({ post }) {
+export default function Post({ post }) {
   return (
     <Theme>
-      <div className='post'>
-        <div className='time'>Published {ms(Date.now() - post.createdAt, { long: true })} ago</div>
+      <div className="post">
+        <div className="time">
+          Published {ms(Date.now() - post.createdAt, { long: true })} ago
+        </div>
         <h1>{post.title}</h1>
-        <div className='content'>
-          {post.content}
+        <div className="content">
+          <Markdown
+            options={{
+              overrides: {
+                Youtube: { component: Youtube },
+              },
+            }}
+          >
+            {post.content}
+          </Markdown>
         </div>
       </div>
     </Theme>
-  )
+  );
 }
 
-export async function getStaticPaths () {
+export async function getStaticPaths() {
+  const MarkdownFiles = await fsPromises.readdir("data");
+
+  const paths = MarkdownFiles.map((filename) => {
+    const slug = filename.replace(/.md$/, "");
+    return {
+      params: { slug },
+    };
+  });
   return {
-    paths: [
-      {
-        params: { slug: '2020-July-01-Hello-World' }
-      }
-    ],
-    fallback: false
-  }
+    paths,
+    fallback: false,
+  };
 }
 
-export async function getStaticProps ({ params }) {
-  const [year, month, day, ...rest] = params.slug.split('-')
-  const createdAt = (new Date(`${year} ${month} ${day}`)).getTime()
-  const title = rest.join(' ')
+export async function getStaticProps({ params }) {
+  const [year, month, day, ...rest] = params.slug.split("-");
+  const createdAt = new Date(`${year} ${month} ${day}`).getTime();
+  const title = rest.join(" ");
+
+  const content = await fsPromises.readFile(`data/${params.slug}.md`, "utf-8");
 
   return {
     props: {
       post: {
         slug: params.slug,
         title,
-        content: `This is the content for ${title}`,
-        createdAt
-      }
-    }
-  }
+        content,
+        createdAt,
+      },
+    },
+  };
 }
